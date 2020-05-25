86a34e47973a6f513fce572ab84dd9e9ea7bf62f
Adil Shalimbekov <adil_s19@mail.ru>
2020-05-24 19:14:43 -0700

    Adding new functionality
    Adding new functionality - clicking Verify button of captcha

--- browseCaptcha.java
+++ browseCaptcha.java
@@ -6,27 +6,45 @@ import java.util.concurrent.TimeUnit;
 
 public class browseCaptcha extends baseChrome{
 
-    public static void main(String[] args) throws MalformedURLException {
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
         AndroidDriver<AndroidElement> driver = Capabilities();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         //Getting a captcha
-        driver.get("https://patrickhlauke.github.io/recaptcha/");
+        driver.get("https://www.google.com/recaptcha/api2/demo");
+
         //Switching to the frame
-        switchToFrame(driver);
+        int number = findFrame(driver, By.xpath("//*[@id=\"recaptcha-anchor\"]/div[1]"));
+        driver.switchTo().frame(number);
+        driver.findElementByXPath("//*[@id=\"recaptcha-anchor\"]/div[1]").click();
+        driver.switchTo().defaultContent();
+
+        Thread.sleep(2000);
+
+        WebElement frame1 = driver.findElement(By.xpath(".//iframe[@title='recaptcha challenge']"));
+        driver.switchTo().frame(frame1);
+        driver.findElement(By.xpath(".//*[@id='recaptcha-verify-button']")).click();
+
+
+//
+//
+//        int number2 = findFrame(driver, By.xpath("//*[@id=\"recaptcha-verify-button\"]"));
+//        driver.switchTo().frame(number2);
+//        driver.findElementByXPath("//*[@id=\"recaptcha-verify-button\"]").click();
+
 
     }
 
-    public static void switchToFrame(WebDriver driver){
+    public static int findFrame(WebDriver driver, By by){
+        int i;
         // Looking for "iframe" in the html, and getting it's size
         int frameCount = driver.findElements(By.tagName("iframe")).size();
         // For loop in order to go through the size
-        for(int i =0;i<frameCount; i++){
+        for(i = 0;i<frameCount; i++){
             driver.switchTo().frame(i);
             // Finding all elements related to captcha
-            int count = driver.findElements(By.xpath("//*[@id=\"recaptcha-anchor\"]/div[1]")).size();
+            int count = driver.findElements(by).size();
             if(count>0){
                 // In case it's found the right element, checking the captcha
-                driver.findElement(By.xpath("//*[@id=\"recaptcha-anchor\"]/div[1]")).click();
                 // Break the loop
                 break;
             }else{
@@ -34,5 +52,7 @@ public class browseCaptcha extends baseChrome{
                 System.out.println("Continue looping");
             }
         }
+        driver.switchTo().defaultContent();
+        return i;
     }
 }
